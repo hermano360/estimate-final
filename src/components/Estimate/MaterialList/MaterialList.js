e44f9b03c80b06261c1c4c396f02b1601c438174
Herminio Garcia <herminio@budsy.io>
2017-12-24 01:24:29 -0800

    material list

---
+++ src/components/Estimate/MaterialList/MaterialList.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import {Modal, Button} from 'react-bootstrap'
+import actions from '../../../redux/actions/actions'
+import request from 'superagent'
+
+import './MaterialList.scss'
+
+export class MaterialList extends Component {
+  constructor(e){
+    super(e)
+    this.state = {
+    }
+  }
+
+  render() {
+    const {quotes,quoteNumber} = this.props
+    console.log(quotes[quoteNumber].shoppingCart)
+
+//     group
+// :
+// "Bathroom faucets"
+// keycode
+// :
+// "Bfaucet1"
+// labor
+// :
+// "$60.00"
+// quantity
+// :
+// "7"
+// sku
+// :
+// "84503"
+// specifications
+// :
+// "Supply Labor And Material For Installation of Moen Adler Chrome 1-handle WaterSense® Bathroom Faucet (Drain Included)  At New Addition Include Supply Flex Lines. *" widespread additional $65.00 lto install ."
+// supplier
+// :
+// "Lowes"
+// template
+// :
+// "Bath2"
+// totalMaterial
+// :
+// "$64.50"
+// uom
+// :
+// "ea"
+//
+// <div>{quotes[quoteNumber].shoppingCart[0].supplier}</div>
+// <div>{quotes[quoteNumber].shoppingCart[0].keycode}</div>
+// <div>{quotes[quoteNumber].shoppingCart[0].sku}</div>
+// <div>{quotes[quoteNumber].shoppingCart[0].uom}</div>
+// <div>{quotes[quoteNumber].shoppingCart[0].totalMaterial}</div>
+// <div>{quotes[quoteNumber].shoppingCart[0].labor}</div>
+// <div>{quotes[quoteNumber].shoppingCart[0].specifications}</div>
+    return (
+      <Modal show={true} onHide={()=>console.log('close')}  className="c-materiallist-modal" >
+        <Modal.Header closeButton>
+          <div className="c-materiallist-header">Material/Labor Cost List</div>
+        </Modal.Header>
+        <Modal.Body>
+        <div className="c-materiallist-pair">
+          <div className="c-materiallist-property">Supplier</div>
+          <div className="c-materiallist-value">{quotes[quoteNumber].shoppingCart[0].supplier}</div>
+        </div>
+        <div className="c-materiallist-pair">
+          <div className="c-materiallist-property">Supplier</div>
+          <div className="c-materiallist-value">{quotes[quoteNumber].shoppingCart[0].keycode}</div>
+        </div>
+        <div className="c-materiallist-pair">
+          <div className="c-materiallist-property">Supplier</div>
+          <div className="c-materiallist-value">{quotes[quoteNumber].shoppingCart[0].sku}</div>
+        </div>
+        <div className="c-materiallist-pair">
+          <div className="c-materiallist-property">Supplier</div>
+          <div className="c-materiallist-value">{quotes[quoteNumber].shoppingCart[0].totalMaterial}</div>
+        </div>
+        <div className="c-materiallist-pair">
+          <div className="c-materiallist-property">Supplier</div>
+          <div className="c-materiallist-value">{quotes[quoteNumber].shoppingCart[0].uom}</div>
+        </div>
+        <div className="c-materiallist-pair">
+          <div className="c-materiallist-property">Supplier</div>
+          <div className="c-materiallist-value">{quotes[quoteNumber].shoppingCart[0].labor}</div>
+        </div>
+        <div className="c-materiallist-pair">
+          <div className="c-materiallist-property">Supplier</div>
+          <div className="c-materiallist-value">{quotes[quoteNumber].shoppingCart[0].specifications}</div>
+        </div>
+
+
+          </Modal.Body>
+        <Modal.Footer>
+          <Button className="c-materiallist-close" onClick={()=>console.log('close')}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+}
+
+export default connect(
+  (state)=>{
+    return {
+      quotes: state.quotes,
+      quoteNumber: state.quoteNumber
+    }
+  }
+)(MaterialList)
---
+++ src/components/Home/Settings/Settings.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import {Modal, Button} from 'react-bootstrap'
+import NumericInput from 'react-numeric-input'
+
+import actions from '../../../redux/actions/actions'
+import './Settings.css'
+
+
+
+class Settings extends Component {
+  constructor(e){
+    super(e)
+    this.handleChangeTax = this.handleChangeTax.bind(this)
+    this.handleChangeExtraWork = this.handleChangeExtraWork.bind(this)
+    this.handleChangeLabor = this.handleChangeLabor.bind(this)
+    this.handleChangeEstimator = this.handleChangeEstimator.bind(this)
+  }
+
+  componentWillMount(){
+    const {dispatch} = this.props
+    if(localStorage.getItem('tax') === undefined || localStorage.getItem('tax') === null){
+      this.handleChangeTax(9.5)
+    } else {
+      dispatch(actions.changeTax(parseFloat(localStorage.getItem('tax'))))
+    }
+    if(localStorage.getItem('labor')=== undefined || localStorage.getItem('labor')=== null){
+      this.handleChangeLabor(30)
+    } else {
+      dispatch(actions.changeLabor(parseFloat(localStorage.getItem('labor'))))
+    }
+    if(localStorage.getItem('extraWork')=== undefined || localStorage.getItem('extraWork')=== null){
+      this.handleChangeExtraWork(40)
+    } else {
+      dispatch(actions.changeExtraWork(parseFloat(localStorage.getItem('extraWork'))))
+    }
+    if(localStorage.getItem('estimator') !== undefined && localStorage.getItem('estimator') !== null){
+      dispatch(actions.changeEstimator(localStorage.getItem('estimator')))
+    }
+  }
+
+  handleChangeTax(value){
+    const {dispatch} = this.props
+    localStorage.setItem('tax', value)
+    dispatch(actions.changeTax(value))
+  }
+  handleChangeExtraWork(value){
+    const {dispatch} = this.props
+    localStorage.setItem('extraWork', value)
+    dispatch(actions.changeExtraWork(value))
+  }
+  handleChangeLabor(value){
+    const {dispatch} = this.props
+    localStorage.setItem('extraLabor', value)
+    dispatch(actions.changeLabor(value))
+  }
+  handleChangeEstimator(value){
+    const {dispatch} = this.props
+    localStorage.setItem('estimator', value.target.value)
+    dispatch(actions.changeEstimator(value.target.value))
+  }
+
+  render() {
+    const {labor, tax, extraWork, showModal, estimator, toggleSettingsModal, dispatch} = this.props
+    return (
+
+      <Modal show={showModal} onHide={toggleSettingsModal} className="c-settings-modal">
+        <Modal.Header closeButton>
+          <div className='c-settings-title'>Settings</div>
+        </Modal.Header>
+        <Modal.Body>
+          <div className='c-settings-label'>Estimator</div>
+          <select className='c-settings-estimator' value={estimator} onChange={this.handleChangeEstimator}>
+            <option value="">-Select-</option>
+            <option value="Arnold Corona">Arnold Corona</option>
+            <option value="Gary Banks">Gary Banks</option>
+            <option value="John Chavez">John Chavez</option>
+            <option value="John Gutierrez">John Gutierrez</option>
+            <option value="Bob Leon">Bob Leon</option>
+            <option value="Ricardo Rivera">Ricardo Rivera</option>
+            <option value="Mike Rogers">Mike Rogers</option>
+            <option value="Cameron Sterling">Cameron Sterling</option>
+          </select>
+          <div className='c-settings-label'>Labor %</div>
+          <NumericInput min={0} max={100} value={labor} step={5}  mobile onChange={this.handleChangeLabor} />
+          <div className='c-settings-label'>Extra Work %</div>
+          <NumericInput min={0} max={100} value={extraWork} step={5}  mobile onChange={this.handleChangeExtraWork} />
+          <div className='c-settings-label'>Tax %</div>
+          <NumericInput min={7} max={15} value={tax} step={0.1} mobile onChange={this.handleChangeTax} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={toggleSettingsModal}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+
+    );
+  }
+}
+
+export default connect(
+  (state)=>{
+    return {
+      labor: state.labor,
+      tax: state.tax,
+      extraWork: state.extraWork,
+      estimator: state.estimator
+    }
+  }
+)(Settings)
