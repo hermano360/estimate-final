700b8b8c5272d86ab043c92743effdf3aa590dfc
Herminio Garcia <hermano360@gmail.com>
2019-11-15 09:49:28 -0800

    fix(group)
    fix group error

--- src/components/Estimate/AddProduct/AddProduct.js
+++ src/components/Estimate/AddProduct/AddProduct.js
@@ -1,196 +1,289 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import {LargeModal} from '../../Common/SimpleModal'
-import actions from '../../../redux/actions/actions'
-import Loadable from 'react-loading-overlay'
-import request from 'superagent'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { LargeModal } from "../../Common/SimpleModal";
+import actions from "../../../redux/actions/actions";
+import Loadable from "react-loading-overlay";
+import request from "superagent";
 
-import baseURL from '../../baseURL'
+import baseURL from "../../baseURL";
 
-import './AddProduct.scss'
+import "./AddProduct.scss";
 
 export class AddProduct extends Component {
   state = {
     loadingProduct: false,
-    group: '',
-    supplier: '',
-    keycode: '',
-    specifications: '',
-    sku: '',
-    totalMaterial: '',
-    labor: '',
-    sortOrder: '',
-    uom: '',
-    'keycodeError': false,
-    'specificationsError': false,
-    'totalMaterialError': false,
-    'laborError': false,
-    'photo': null
-  }
+    group: "",
+    supplier: "",
+    keycode: "",
+    specifications: "",
+    sku: "",
+    totalMaterial: "",
+    labor: "",
+    sortOrder: "",
+    uom: "",
+    keycodeError: false,
+    specificationsError: false,
+    totalMaterialError: false,
+    laborError: false,
+    photo: null
+  };
 
   cleanState = {
-    group: '',
-    supplier: '',
-    keycode: '',
-    specifications: '',
-    sku: '',
-    totalMaterial: '',
-    labor: '',
-    sortOrder: '',
-    uom: '',
-    'keycodeError': false,
-    'specificationsError': false,
-    'totalMaterialError': false,
-    'laborError': false,
-    'photo': null
-  }
+    group: "",
+    supplier: "",
+    keycode: "",
+    specifications: "",
+    sku: "",
+    totalMaterial: "",
+    labor: "",
+    sortOrder: "",
+    uom: "",
+    keycodeError: false,
+    specificationsError: false,
+    totalMaterialError: false,
+    laborError: false,
+    photo: null
+  };
 
   updateProductProperty = (property, e) => {
-    const newState = { [property]: e.target.value }
+    const newState = { [property]: e.target.value };
 
-    if(['keycode', 'specifications','totalMaterial','labor'].includes(property)){
-      newState[`${property}Error`] = false
+    if (
+      ["keycode", "specifications", "totalMaterial", "labor"].includes(property)
+    ) {
+      newState[`${property}Error`] = false;
     }
-    this.setState(newState)
-  }
+    this.setState(newState);
+  };
 
-  formateDate = (date) => {
-    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
-  }
-
-
+  formateDate = date => {
+    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+  };
 
   handleToggle = () => {
-    const {toggleAddProduct} = this.props
-    this.setState((prevState) => {
+    const { toggleAddProduct } = this.props;
+    this.setState(prevState => {
       return {
         ...prevState,
         ...this.cleanState
-      }
-    })
-    toggleAddProduct()
-  }
-  validateSubmissionValues = (keycode,specifications,sku,uom,totalMaterial,labor) => {
-    let errorObject = []
-    if(keycode ==='') errorObject['keycodeError'] = true
-    if(specifications ==='') errorObject['specificationsError'] = true
-    if(totalMaterial ==='') errorObject['totalMaterialError'] = true
-    if(labor==='') errorObject['laborError'] = true
+      };
+    });
+    toggleAddProduct();
+  };
+  validateSubmissionValues = (
+    keycode,
+    specifications,
+    sku,
+    uom,
+    totalMaterial,
+    labor
+  ) => {
+    let errorObject = [];
+    if (keycode === "") errorObject["keycodeError"] = true;
+    if (specifications === "") errorObject["specificationsError"] = true;
+    if (totalMaterial === "") errorObject["totalMaterialError"] = true;
+    if (labor === "") errorObject["laborError"] = true;
 
-    return errorObject
-  }
+    return errorObject;
+  };
   handleSubmit = () => {
-    const {baseURL, authToken, getProducts} = this.props
-    const {group,supplier,keycode,specifications,sku,uom,totalMaterial,labor,sortOrder} = this.state
-    const validationErrors = this.validateSubmissionValues(keycode,specifications,sku,uom,totalMaterial,labor)
-    if(Object.keys(validationErrors).length === 0){
-      this.setState({loadingProduct: true})
+    const { baseURL, authToken, getProducts } = this.props;
+    const {
+      group,
+      supplier,
+      keycode,
+      specifications,
+      sku,
+      uom,
+      totalMaterial,
+      labor,
+      sortOrder
+    } = this.state;
+    const validationErrors = this.validateSubmissionValues(
+      keycode,
+      specifications,
+      sku,
+      uom,
+      totalMaterial,
+      labor
+    );
+    if (Object.keys(validationErrors).length === 0) {
+      this.setState({ loadingProduct: true });
       request
-      .post(`${baseURL}/products/add`)
-      .send({
-        authToken,
-        product : {
-          updated: this.formateDate(new Date),
-          misc: '$0.00',
-          group,
-          supplier,
-          keycode,
-          specifications,
-          sku,
-          uom: uom ==='' ? 'each' : uom,
-          materialCost: `$${Number(totalMaterial).toFixed(2)}`,
-          totalMaterial: `$${Number(totalMaterial).toFixed(2)}`,
-          labor: `$${Number(labor).toFixed(2)}`,
-          sortOrder
-        }
-      })
-      .then(res =>{
-        this.setState({loadingProduct: false})
-        this.handleToggle()
-        getProducts()
-      }).catch(err => {
-        console.log(err)
-      })
+        .post(`${baseURL}/products/add`)
+        .send({
+          authToken,
+          product: {
+            updated: this.formateDate(new Date()),
+            misc: "$0.00",
+            group,
+            supplier,
+            keycode,
+            specifications,
+            sku,
+            uom: uom === "" ? "each" : uom,
+            materialCost: `$${Number(totalMaterial).toFixed(2)}`,
+            totalMaterial: `$${Number(totalMaterial).toFixed(2)}`,
+            labor: `$${Number(labor).toFixed(2)}`,
+            sortOrder
+          }
+        })
+        .then(res => {
+          this.setState({ loadingProduct: false });
+          this.handleToggle();
+          getProducts();
+        })
+        .catch(err => {
+          console.log(err);
+        });
     } else {
       this.setState({
         ...validationErrors
-      })
+      });
     }
-  }
+  };
 
   renderGroupOptions = () => {
-    let groups = []
-    let allProducts = JSON.parse(localStorage.getItem('products'))
+    let groups = [];
+    let allProducts = JSON.parse(localStorage.getItem("products"));
     allProducts.forEach(product => {
-    if(!groups.includes(product.group)){
-            groups.push(product.group)
-        }
-    })
+      if (!groups.includes(product.group)) {
+        groups.push(product.group);
+      }
+    });
 
     return groups
-    .map(group => {
-      return group === '' ? '' : group[0].toUpperCase() + group.slice(1)
-    })
-    .filter((group,i) => {
-      return group !== '' && groups.indexOf(group) === i
-    })
-    .sort()
-    .map(group => {
-      return (
-        <option key={group }value={group}>{group}</option>
-      )
-    })
-  }
-
+      .filter(group => group)
+      .map(group => {
+        return group === "" ? "" : group[0].toUpperCase() + group.slice(1);
+      })
+      .filter((group, i) => {
+        return group !== "" && groups.indexOf(group) === i;
+      })
+      .sort()
+      .map(group => {
+        return (
+          <option key={group} value={group}>
+            {group}
+          </option>
+        );
+      });
+  };
 
   render() {
-    const {show, toggleAddProduct, getProducts} = this.props
+    const { show, toggleAddProduct, getProducts } = this.props;
     const {
-      loadingProduct, group, supplier, keycode,
-      specifications, sku, uom, totalMaterial,
-      labor, sortOrder, laborError, keycodeError,
-      totalMaterialError, specificationsError, photo} = this.state
+      loadingProduct,
+      group,
+      supplier,
+      keycode,
+      specifications,
+      sku,
+      uom,
+      totalMaterial,
+      labor,
+      sortOrder,
+      laborError,
+      keycodeError,
+      totalMaterialError,
+      specificationsError,
+      photo
+    } = this.state;
     return (
-      <LargeModal open={show} toggle={this.handleToggle} className="c-addproduct-modal" >
+      <LargeModal
+        open={show}
+        toggle={this.handleToggle}
+        className="c-addproduct-modal"
+      >
         <Loadable
           active={loadingProduct}
           spinner
-          text={`Updating Product Info`}>
+          text={`Updating Product Info`}
+        >
           <div className="c-addproduct-body">
             <div className="c-addproduct-header">Add Product</div>
             <div className="c-addproduct-group">
               <div className="c-addproduct-prop">Desired Key Code</div>
-              <input type="text" className={`c-addproduct-value ${keycodeError ? 'error' : ''}`} value={keycode} onChange={ e => this.updateProductProperty('keycode', e)}/>
+              <input
+                type="text"
+                className={`c-addproduct-value ${keycodeError ? "error" : ""}`}
+                value={keycode}
+                onChange={e => this.updateProductProperty("keycode", e)}
+              />
             </div>
             <div className="c-addproduct-group">
               <div className="c-addproduct-prop">Photo</div>
-              <label htmlFor="product-photo" className={`c-addproduct-prop photo-label ${photo ? 'uploaded': ''}`}>{photo ? 'Uploaded':'Upload...'}</label>
-              <input type="file" accept="image/*" id="product-photo" onChange={e=>this.setState({photo: e.target.files[0]})}/>
+              <label
+                htmlFor="product-photo"
+                className={`c-addproduct-prop photo-label ${
+                  photo ? "uploaded" : ""
+                }`}
+              >
+                {photo ? "Uploaded" : "Upload..."}
+              </label>
+              <input
+                type="file"
+                accept="image/*"
+                id="product-photo"
+                onChange={e => this.setState({ photo: e.target.files[0] })}
+              />
             </div>
             <div className="c-addproduct-group">
               <div className="c-addproduct-prop">Specifications</div>
-              <textarea className={`c-addproduct-value ${specificationsError ? 'error' : ''}`} rows="4" value={specifications} onChange={ e => this.updateProductProperty('specifications', e)}>
-              </textarea>
+              <textarea
+                className={`c-addproduct-value ${
+                  specificationsError ? "error" : ""
+                }`}
+                rows="4"
+                value={specifications}
+                onChange={e => this.updateProductProperty("specifications", e)}
+              ></textarea>
             </div>
             <div className="c-addproduct-group">
               <div className="c-addproduct-prop">SKU</div>
-              <input type="text" className="c-addproduct-value" value={sku} onChange={ e => this.updateProductProperty('sku', e)}/>
+              <input
+                type="text"
+                className="c-addproduct-value"
+                value={sku}
+                onChange={e => this.updateProductProperty("sku", e)}
+              />
             </div>
             <div className="c-addproduct-group">
               <div className="c-addproduct-prop">Unit of Measure</div>
-              <input type="text" className="c-addproduct-value" value={uom} onChange={ e => this.updateProductProperty('uom', e)}/>
+              <input
+                type="text"
+                className="c-addproduct-value"
+                value={uom}
+                onChange={e => this.updateProductProperty("uom", e)}
+              />
             </div>
             <div className="c-addproduct-group">
               <div className="c-addproduct-prop">Material Cost</div>
-              <input type="number" className={`c-addproduct-value ${totalMaterialError ? 'error' : ''}`} value={totalMaterial} onChange={ e => this.updateProductProperty('totalMaterial', e)}/>
+              <input
+                type="number"
+                className={`c-addproduct-value ${
+                  totalMaterialError ? "error" : ""
+                }`}
+                value={totalMaterial}
+                onChange={e => this.updateProductProperty("totalMaterial", e)}
+              />
             </div>
             <div className="c-addproduct-group">
               <div className="c-addproduct-prop">Labor Cost</div>
-              <input type="number" className={`c-addproduct-value ${laborError ? 'error' : ''}`} value={labor} onChange={ e => this.updateProductProperty('labor', e)}/>
+              <input
+                type="number"
+                className={`c-addproduct-value ${laborError ? "error" : ""}`}
+                value={labor}
+                onChange={e => this.updateProductProperty("labor", e)}
+              />
             </div>
             <div className="c-addproduct-group">
               <div className="c-addproduct-prop">Add to Existing Group?</div>
-              <select className="c-addproduct-value" value={group} onChange={ e => this.updateProductProperty('group', e)}>
+              <select
+                className="c-addproduct-value"
+                value={group}
+                onChange={e => this.updateProductProperty("group", e)}
+              >
                 <option value="">-Select-</option>
                 {this.renderGroupOptions()}
                 <option value="other">Other</option>
@@ -198,7 +291,11 @@ export class AddProduct extends Component {
             </div>
             <div className="c-addproduct-group">
               <div className="c-addproduct-prop">Supplier</div>
-              <select className="c-addproduct-value" value={supplier} onChange={ e => this.updateProductProperty('supplier', e)}>
+              <select
+                className="c-addproduct-value"
+                value={supplier}
+                onChange={e => this.updateProductProperty("supplier", e)}
+              >
                 <option value="">-Select-</option>
                 <option value="Home Depot">Home Depot</option>
                 <option value="Lowes">Lowes</option>
@@ -208,7 +305,12 @@ export class AddProduct extends Component {
             </div>
             <div className="c-addproduct-group">
               <div className="c-addproduct-prop">Sort Order</div>
-              <input type="number" className="c-addproduct-value" value={sortOrder} onChange={ e => this.updateProductProperty('sortOrder', e)}/>
+              <input
+                type="number"
+                className="c-addproduct-value"
+                value={sortOrder}
+                onChange={e => this.updateProductProperty("sortOrder", e)}
+              />
             </div>
             <div className="c-addproduct-submit" onClick={this.handleSubmit}>
               Submit
@@ -216,8 +318,8 @@ export class AddProduct extends Component {
           </div>
         </Loadable>
       </LargeModal>
-    )
+    );
   }
 }
 
-export default AddProduct
+export default AddProduct;
